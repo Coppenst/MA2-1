28a0846bfb6aa472e3a3a775fd8fbcbaa6fa5917
Coppenst <markhagelauer@gmail.com>
2016-02-21 21:56:59 +0100

    Implementing store method for the makers
    - Createmaker class with postman

--- app/Http/Controllers/MakerController.php
+++ app/Http/Controllers/MakerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Maker;
+use App\Http\Requests\CreateMakerRequest;
 
 class MakerController extends Controller {
 
@@ -26,8 +27,15 @@ class MakerController extends Controller {
 	 *
 	 * @return Response
 	 */
-	public function store()
+	public function store(CreateMakerRequest $request)
 	{
+		$values = $request->only(['name', 'phone']);
+		
+		Maker::create($values);
+
+		return response()->json(['message'=>'Maker correclty added'],201);
+
+
 		//
 	}
 
